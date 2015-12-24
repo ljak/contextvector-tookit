5ab411f63b0a8276c4a97da812848a9241e7a6d9
ljak <laurentjakubina@gmail.com>
2015-12-23 23:05:11 -0500

    similarity measure class enum and command line interface (cli) 1 version done

---
+++ src/org/rali/ljak/ecva/align/SimilarityMeasures.java
@@ -0,0 +1,29 @@
+package org.rali.ljak.ecva.align;
+
+public enum SimilarityMeasures {
+
+	COS {
+		@Override
+		public String getName() {
+			return "Cosine";
+		}
+
+		@Override
+		public String getAcronyme() {
+			return "COS";
+		}
+
+		@Override
+		public double compute() {
+			return 0;
+		}
+	};
+	
+	/**
+	 * Contract Template for all Association Measure.
+	 */
+	public abstract String getName();
+	public abstract String getAcronyme();
+    public abstract double compute();
+    
+}
